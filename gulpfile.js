2d05c67cf8641aca960a2492402a28c3e02599e6
whariharan <whariharan@gmail.com>
2016-12-12 08:33:42 +0530

    Added Cofee Scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp=require('gulp'),
-	gUtil=require('gulp-util');
+	gUtil=require('gulp-util'),
+	coffee=require('gulp-coffee');
 
 
 gulp.task('log',function(){
 	gUtil.log('Chasing for good things');
+});
+
+var coffeeSources=['components/coffee/tagline.coffee'];
+gulp.task('coffee',function(){
+	gulp.src(coffeeSources)
+	.pipe(coffee({bare: true}))
+		.on('error',gUtil.log)
+	.pipe(gulp.dest('components/scripts'));
 });
