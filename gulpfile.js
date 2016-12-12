ec21989d35de875032053b3f91f1dd862b1279b1
whariharan <whariharan@gmail.com>
2016-12-12 08:43:06 +0530

    Common JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp=require('gulp'),
 	gUtil=require('gulp-util'),
-	coffee=require('gulp-coffee');
+	coffee=require('gulp-coffee'),
+	concat=require('gulp-concat');
 
 
 gulp.task('log',function(){
@@ -8,9 +9,21 @@ gulp.task('log',function(){
 });
 
 var coffeeSources=['components/coffee/tagline.coffee'];
+var jsSources=[
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'];
+
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
 	.pipe(coffee({bare: true}))
 		.on('error',gUtil.log)
 	.pipe(gulp.dest('components/scripts'));
+});
+
+gulp.task('js',function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'));
 });
