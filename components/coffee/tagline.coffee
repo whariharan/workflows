d9d35dc6340812debe4773e1accd1557566f371d
whariharan <whariharan@gmail.com>
2016-12-16 06:08:58 +0530

    Gulp Watch

--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,5 +1,5 @@
 $=require 'jquery'
 
-do fill = (item = 'The most creative minds in Art') ->
+do fill = (item = 'The most creative minds Art') ->
   $('.tagline').append "#{item}"
 fill
--- gulpfile.js
+++ gulpfile.js
@@ -44,3 +44,9 @@ gulp.task('compass',function(){
 });
 
 gulp.task('default',['coffee','js','compass']);
+
+gulp.task('watch',function(){
+	gulp.watch(coffeeSources,['coffee']);
+	gulp.watch(jsSources,['js']);
+	gulp.watch('components/sass/*.scss',['compass']);
+});
